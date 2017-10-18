eb87f3c8cedf0336b8c499958cbb33774583c983
ASUS\Dayllen Effendi <day_llen_effendi@hotmail.com>
2017-10-18 16:34:23 +0700

    uhjbu

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login','UserController@showLoginForm')->name('login');
+Route::post('/login','UserController@login')->name('login.submit');
+
+Route::get('/register','UserController@register')->name('register');
+
+Route::post('/upload','UserController@upload')->name('upload');
+
+Route::put('/edit','Auth\HomeController@edit')->name('edit');
+
+Route::delete('/delete'.'Auth\HomeController@edit')->name('delete');
